c23a403c73d02ae753d395d4e76ff1e685d76b86
Gil Kartaginer <73134488+gil-kart@users.noreply.github.com>
2022-02-28 12:14:49 +0200

    Add files via upload

---
+++ example/src/main/java/org/example/ExampleClass.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class ExampleClass {
+    public static void main(String[] args) {
+        System.out.println("sfsdjlfhsdkfjsd");
+    }
+
+    public ExampleClass() {
+    }
+}
