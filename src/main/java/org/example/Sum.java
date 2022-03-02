470ac9e5268e857b7cc37a41a072109b0351cf41
gil-kart <73134488+gil-kart@users.noreply.github.com>
2022-03-02 15:49:19 +0200

    rearranged code

--- src/main/java/org/example/Sum.java
+++ src/main/java/org/example/Sum.java
@@ -1,37 +1,70 @@
 package org.example;
-
 import java.util.*;
 
 public class Sum {
     public static void main(String[] Args) {
-        // Allow user input
-        boolean input_not_correct = true;
-        int base = 10;
-        Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter base (2/8/10/16):");
-        while(input_not_correct) {
-            base = input.nextInt();
-            if (base != 2 && base != 8 && base != 10 && base != 16) {
-                System.out.println("Error - this base isn\'t supported. Please enter a base (2/8/10/16):\n");
+        Input input = get_input();
+        if(!check_expression_valid(input))
+            return;
+        Expression exp = parse_expression(input.expression);
+        if(wrong_base_digits(exp, input.base))
+            return;
+        List<Float> list_of_num = create_list_of_decimals(exp, input.base);
+        float res = calc_in_decimal(list_of_num, exp.signs);
+        print_result_in_orig_base(res, input);
+
+    }
+
+    private static void print_result_in_orig_base(float res, Input input) {
+        String result="";
+        if(input.base == 2)
+            result = Integer.toBinaryString((int) res);
+        else if(input.base == 8)
+            result = Integer.toOctalString((int) res);
+        else if(input.base == 10)
+            result = String.valueOf((int) res);
+        else if(input.base == 16)
+            result = Integer.toHexString((int) res);
+        System.out.println("The value of expression " + input.orig_expression + " is: " + result.toUpperCase());
+
+    }
+
+
+    private static Boolean check_expression_valid(Input input) {
+        if(division_by_zero(input.expression)){
+            System.out.println("Error: trying to divide by 0 (evaluated: \"0\")");
+            return false;
+        }
+        if(invalid_expression(input.expression)){
+            System.out.println("Error: invalid expression: \"\"");
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean wrong_base_digits(Expression exp, int base) {
+        for(int i=0; i<exp.terminals.size() ; i++) {
+            char[] check = exp.terminals.get(i);
+            String str = new String(check);
+            try{
+                Integer.valueOf(str, base);
             }
-            else{
-                input_not_correct = false;
+            catch (Exception e){
+                System.out.println("Error: invalid expression: " + "\"" + str + "\"");
+                return true;
             }
         }
+        return false;
+    }
 
-        System.out.println("Enter expression:");
-        String expression = input.nextLine();
-        expression = input.nextLine();
-        String save_expression = expression;
-        expression = expression.replaceAll(" ","");
-
-        List<char[]> terminals =new ArrayList<char[]>();
+    private static Expression parse_expression(String expression) {
+        List<char[]> terminals =new ArrayList<>();
         List<Character> signs = new ArrayList<>();
 
         int len = expression.length();
         int last_sign_index = 0;
-        float res=0;
+
         for(int i=0; i<len ; i++){
             if(expression.charAt(i) == '+' || expression.charAt(i) == '-' || expression.charAt(i) == '*' || expression.charAt(i) == '/'){
                 signs.add(expression.charAt(i));
@@ -48,51 +81,34 @@ public class Sum {
                 last_sign_index = i;
             }
         }
-        if(division_by_zero(expression)){
-            System.out.println("Error: trying to divide by 0 (evaluated: \"0\")");
-            input.close();
-            return;
-        }
-        if(invalid_expression(expression)){
-            System.out.println("Error: invalid expression: \"\"");
-            input.close();
-            return;
-        }
-
         char[] terminal = new char[len - last_sign_index - 1];
         expression.getChars(last_sign_index + 1, len, terminal, 0);
         terminals.add(terminal);
 
+        return new Expression(terminals, signs);
+    }
 
-        List<Float> list_of_num = new ArrayList<Float>();
-        for(int i=0; i<terminals.size() ; i++) {
-            char[] check = terminals.get(i);
-            String str = new String(check);
-            float number=0;
-            try{
-                number = Integer.valueOf(str, base);
+    private static Input get_input() {
+        boolean input_not_correct = true;
+        int base = 10;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter base (2/8/10/16):");
+        while(input_not_correct) {
+            base = input.nextInt();
+            if (base != 2 && base != 8 && base != 10 && base != 16) {
+                System.out.println("Error - this base isn\'t supported. Please enter a base (2/8/10/16):");
             }
-            catch (Exception e){
-                System.out.println("Error: invalid expression: " + "\"" + str + "\"");
-                input.close();
-                return;
+            else{
+                input_not_correct = false;
             }
-            list_of_num.add(number);
         }
-        res = calc_in_decimal(list_of_num, signs);
 
-        String result="";
-        if(base == 2)
-            result = Integer.toBinaryString((int) res);
-        else if(base == 8)
-            result = Integer.toOctalString((int) res);
-        else if(base == 10)
-            result = String.valueOf((int) res);
-        else if(base == 16)
-            result = Integer.toHexString((int) res);
-        System.out.println("The value of expression " + save_expression + " is: " + result.toUpperCase());
-
+        System.out.println("Enter expression:");
+        String expression = input.nextLine();
+        String orig_expression = input.nextLine();
+        expression = orig_expression.replaceAll(" ","");
         input.close();
+        return new Input(base, expression, orig_expression);
     }
 
     public static Boolean division_by_zero(String expression){
@@ -109,7 +125,23 @@ public class Sum {
         return false;
     }
 
-
+    public static List<Float> create_list_of_decimals(Expression exp, int base){
+        List<Float> list_of_num = new ArrayList<>();
+        for(int i=0; i<exp.terminals.size() ; i++) {
+            char[] check = exp.terminals.get(i);
+            String str = new String(check);
+            float number;
+            try{
+                number = Integer.valueOf(str, base);
+            }
+            catch (Exception e){
+                System.out.println("Error: invalid expression: " + "\"" + str + "\"");
+                return list_of_num;
+            }
+            list_of_num.add(number);
+        }
+        return list_of_num;
+    }
     public static float calc_in_decimal(List<Float> list_of_num, List<Character> signs) {
         for(int i=0; i<signs.size();i++){
             if(signs.get(i).equals('*')||signs.get(i).equals('/')){
@@ -134,5 +166,27 @@ public class Sum {
                 res -= right_var;
         }
         return res;
+    }
+}
+
+class Input {
+    int base;
+    String expression;
+    String orig_expression;
+    Input(int _base, String _expression,String _orig_expression)
+    {
+        base = _base;
+        expression = _expression;
+        orig_expression =  _orig_expression;
+    }
+}
+
+class Expression {
+    List<char[]> terminals;
+    List<Character> signs;
+    Expression(List<char[]> _terminals,List<Character> _signs)
+    {
+        terminals = _terminals;
+        signs = _signs;
     }
 }
